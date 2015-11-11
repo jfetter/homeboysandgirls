1e75d75e4c4eda560827b7faff5e5b71d942b018
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-10 22:03:13 -0800

    I can find my contactsgit add .git add .

--- models/contact.js
+++ models/contact.js
@@ -1,17 +1,21 @@
 //model
 "use strict"
-var file = require("file-system");
+//this version of fs is async
+//var file = require("file-system");
+//this version is synchronous
 var fs = require("fs");
 
 var Contact = {};
 
 Contact.find = function(cb){
-	fs.readFile("/db/contacts.json", function (err, data){
+	fs.readFile("contacts.json", function (err, data){
+		console.log("hey you got to model")
 		if(err){
 			cb(err);
 		} else{
+			console.log(data)
 			var contacts = JSON.parse(data);
-			cb(null, todos);
+			cb(null, contacts);
 		}
 	})
 }
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,7 @@
 "use strict";
 
 app.get("/", function(req, res){
+	console.log("in the route")
 	res.send("I'm going to send you contact info \n")
 })
 
